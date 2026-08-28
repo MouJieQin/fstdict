1fb4c2adac1ea613d127076726a37224c166ad40
MouJieQin <724174637@qq.com>
2026-08-28 22:31:21 +0800

    Fix: extra psutil code in python

--- src-python/libs/handlers/exit_handler.py
+++ src-python/libs/handlers/exit_handler.py
@@ -1,19 +1,10 @@
 import os
 import asyncio
-import psutil
 from libs.log_config import logger
 from libs.common.utils import Utils
 
 
 class ExitHandler:
-    @staticmethod
-    def is_pid_alive(pid: int) -> bool:
-        return psutil.pid_exists(pid)
-
-    @staticmethod
-    def check_tauri_main_alive() -> bool:
-        return ExitHandler.is_pid_alive(Utils.tauri_main_pid) if Utils.tauri_main_pid != 0 else False
-
     @staticmethod
     async def _notify_cgevent_server_exit():
         if Utils.cgevent_ws_client:
