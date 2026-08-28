d8094eaf9bab31f2e867169220d2323d790bee7b
MouJieQin <724174637@qq.com>
2026-08-28 13:36:38 +0800

    Fix:Some chunks are larger than 500 kB after minification

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import i18n from '@/i18n'
@@ -13,7 +12,6 @@ const app = createApp(App)
 const pinia = createPinia()
 
 app.use(router)
-app.use(ElementPlus)
 app.use(pinia)
 app.use(i18n)
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
+import { visualizer } from 'rollup-plugin-visualizer';
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -12,7 +16,39 @@ export default defineConfig(async () => ({
   },
   base: './',
 
-  plugins: [vue()],
+  plugins: [vue(),
+  AutoImport({
+    resolvers: [ElementPlusResolver()],
+  }),
+  Components({
+    resolvers: [ElementPlusResolver()],
+  }),
+  visualizer({ open: false })
+  ],
+
+  build: {
+    // Vite 8 uses Rolldown configurations here
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: 'icons-vendor',
+              // Target vue-icons-plus package files
+              test: /node_modules[\\/]vue-icons-plus/,
+              priority: 20,
+            },
+            {
+              name: 'element-vendor',
+              // Target element-plus UI components to lighten index.js further
+              test: /node_modules[\\/]element-plus/,
+              priority: 10,
+            }
+          ]
+        }
+      }
+    }
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -27,7 +63,7 @@ export default defineConfig(async () => ({
 
     watch: {
       // 3. tell Vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**", 'node_modules'],
+      ignored: ["**/src-tauri/**", 'node_modules', 'src-python', 'src-helper'],
     },
   },
 }));
